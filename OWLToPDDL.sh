f35febe90b8c3a60f0dcb063e7e2637a69836b56
Gustavo <g.rezendesilva@tudelft.nl>
2025-03-09 20:22:38 +0100

    update OWLToPDDL to use fullpath

--- OWLToPDDL.sh
+++ OWLToPDDL.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-java -jar build/libs/dlToPlanning-1.0-SNAPSHOT-all.jar "$@"
+# Resolve the script's directory
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Run the JAR with an absolute path
+java -jar "$SCRIPT_DIR/build/libs/dlToPlanning-1.0-SNAPSHOT-all.jar" "$@"
